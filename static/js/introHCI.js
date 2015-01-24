94a6374359d3c4c07c12903c714513cac331617a
Sachi Pitkin <spitkin@ucsd.edu>
2015-01-24 20:31:03 +0000

    fixing for heroku upload2

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,9 @@
 'use strict';
-
+/*
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+})*/
 
 /*
  * Function that is called when the document is ready.
